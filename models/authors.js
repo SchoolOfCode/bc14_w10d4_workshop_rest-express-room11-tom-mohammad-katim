7aa5daea0ffc81754553adc635c370f36aadb38e
Katim Touray <katim70@hotmail.co.uk>
2023-05-18 16:24:58 +0100

    task 3

--- models/authors.js
+++ models/authors.js
@@ -1,21 +1,18 @@
 import { pool } from "../db/index.js";
-
 export async function getAuthors() {
   // Query the database and return all authors
   const { rows } = await pool.query("SELECT * FROM authors");
   console.table(rows);
   return rows;
 }
-
 export async function searchAuthorsByName(searchTerm) {
   // Query the database and return all authors that have a name matching the searchTerm
   const { rows } = await pool.query(
-    "SELECT * FROM authors WHERE first_name || last_name ILIKE $1",
+    "SELECT * FROM authors WHERE first_name || ' ' || last_name ILIKE $1",
     [`%${searchTerm}%`]
   );
   return rows;
 }
-
 export async function getAuthorById(id) {
   // Query the database and return the book with a matching id
   const { rows } = await pool.query("SELECT * FROM authors WHERE id=$1", [id]);
@@ -27,23 +24,21 @@ export async function getAuthorById(id) {
     return null;
   }
 }
-
 export async function createAuthor(author) {
   // Query the database to create an author and return the newly created author
-  const { name, bio } = author;
+  const { first_name, last_name } = author;
   const { rows } = await pool.query(
-    "INSERT INTO authors (name, bio) VALUES ($1, $2) RETURNING *",
-    [name, bio]
+    "INSERT INTO authors (first_name, last_name) VALUES ($1, $2) RETURNING *",
+    [first_name, last_name]
   );
   return rows[0];
 }
-
 export async function updateAuthorById(id, updates) {
   // Query the database to update an author and return the newly updated author
-  const { name, bio } = updates;
+  const { first_name, last_name } = updates;
   const { rows } = await pool.query(
-    "UPDATE authors SET name=$1, bio=$2 WHERE id=$3 RETURNING *",
-    [name, bio, id]
+    "UPDATE authors SET first_name=$1, last_name=$2 WHERE id=$3 RETURNING *",
+    [first_name, last_name, id]
   );
   if (rows.length > 0) {
     return rows[0];
@@ -51,16 +46,4 @@ export async function updateAuthorById(id, updates) {
     return null;
   }
 }
-
-export async function deleteAuthorById(id) {
-  // Query the database to delete an author and return the deleted author
-  const { rows } = await pool.query(
-    "DELETE FROM authors WHERE id=$1 RETURNING *",
-    [id]
-  );
-  if (rows.length > 0) {
-    return rows[0];
-  } else {
-    return null;
-  }
-}
+rr
